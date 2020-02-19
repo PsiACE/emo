0520d0d53ed1d54a4044a729574115e43047939b
Chojan Shang <psiace@outlook.com>
2020-02-19 17:19:35 +0800

    :art: Apply editor rules.

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,4 +2,5 @@
 
 clang-format -i "${MESON_SOURCE_ROOT}"/src/*.c
 clang-format -i "${MESON_SOURCE_ROOT}"/src/*/*.c
+clang-format -i "${MESON_SOURCE_ROOT}"/include/*/*.h
 clang-format -i "${MESON_SOURCE_ROOT}"/include/*/*/*.h
